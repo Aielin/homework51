5e59c5f3d1957242f34a02c44fba6a6674f6a7f4
Aielina Akylbekova <aielina.madambekova@gmail.com>
2024-09-12 11:20:00 +0600

    Fixed issues with random number generation and display logic

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,23 @@ import {useState} from "react";
 import NumberBalls from "./Components/NumberBalls.tsx";
 
 const App = () =>  {
-    const [numbers, setNumbers] = useState<number[]>(generateNumbers());
+    const [numbers, setNumbers] = useState<number[]>([5, 11, 16, 23, 32]);
+
+    const generateNumbers = () => {
+        const numbers = new Set<number>();
+        while (numbers.size < 5) {
+            const randomNumber = Math.floor(Math.random() * (36 - 5 + 1)) + 5;
+            numbers.add(randomNumber);
+        }
+        return Array.from(numbers).sort((a, b) => a - b);
+    };
 
     return (
         <>
             <button type={"button"} onClick={() => setNumbers(generateNumbers())}>New numbers</button>
             <NumberBalls numbers={numbers} />
         </>
-    )
-}
-
-const generateNumbers = () => Array.from({ length: 5 }, () => Math.floor(Math.random() * 100))
+    );
+};
 
 export default App
--- src/Components/NumberBalls.tsx
+++ src/Components/NumberBalls.tsx
@@ -2,12 +2,17 @@ import '../App.css';
 import React from 'react';
 import NumberBall from "./NumberBall.tsx";
 interface Props {
-    numbers: Array<number>;
+    numbers: number[];
 }
 
-const NumberBalls: React.FC<Props> = props =>{
-    return  props.numbers.map((number, index) => (<NumberBall number={number} key={index}  />))
-
-}
+const NumberBalls: React.FC<Props> = (props) => {
+    return (
+        <>
+            {props.numbers.map((number, index) => (
+                <NumberBall number={number} key={index} />
+            ))}
+        </>
+    );
+};
 
 export default NumberBalls;
